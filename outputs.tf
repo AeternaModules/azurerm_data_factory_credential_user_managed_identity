6175425c813e7c6b3929d59fdd22dc0181b21f18
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:41:55 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "data_factory_credential_user_managed_identitys" {
+  description = "All data_factory_credential_user_managed_identity resources"
+  value       = azurerm_data_factory_credential_user_managed_identity.data_factory_credential_user_managed_identitys
+}
+

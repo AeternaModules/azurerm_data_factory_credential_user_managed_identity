f51136e8f81845ccaca3823b4424ac44a45e121a
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:47:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "data_factory_credential_user_managed_identities_id" {
   description = "Map of id values across all data_factory_credential_user_managed_identities, keyed the same as var.data_factory_credential_user_managed_identities"
-  value       = { for k, v in azurerm_data_factory_credential_user_managed_identity.data_factory_credential_user_managed_identities : k => v.id }
+  value       = { for k, v in azurerm_data_factory_credential_user_managed_identity.data_factory_credential_user_managed_identities : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "data_factory_credential_user_managed_identities_annotations" {
   description = "Map of annotations values across all data_factory_credential_user_managed_identities, keyed the same as var.data_factory_credential_user_managed_identities"
-  value       = { for k, v in azurerm_data_factory_credential_user_managed_identity.data_factory_credential_user_managed_identities : k => v.annotations }
+  value       = { for k, v in azurerm_data_factory_credential_user_managed_identity.data_factory_credential_user_managed_identities : k => v.annotations if v.annotations != null && length(v.annotations) > 0 }
 }
 output "data_factory_credential_user_managed_identities_data_factory_id" {
   description = "Map of data_factory_id values across all data_factory_credential_user_managed_identities, keyed the same as var.data_factory_credential_user_managed_identities"
-  value       = { for k, v in azurerm_data_factory_credential_user_managed_identity.data_factory_credential_user_managed_identities : k => v.data_factory_id }
+  value       = { for k, v in azurerm_data_factory_credential_user_managed_identity.data_factory_credential_user_managed_identities : k => v.data_factory_id if v.data_factory_id != null && length(v.data_factory_id) > 0 }
 }
 output "data_factory_credential_user_managed_identities_description" {
   description = "Map of description values across all data_factory_credential_user_managed_identities, keyed the same as var.data_factory_credential_user_managed_identities"
-  value       = { for k, v in azurerm_data_factory_credential_user_managed_identity.data_factory_credential_user_managed_identities : k => v.description }
+  value       = { for k, v in azurerm_data_factory_credential_user_managed_identity.data_factory_credential_user_managed_identities : k => v.description if v.description != null && length(v.description) > 0 }
 }
 output "data_factory_credential_user_managed_identities_identity_id" {
   description = "Map of identity_id values across all data_factory_credential_user_managed_identities, keyed the same as var.data_factory_credential_user_managed_identities"
-  value       = { for k, v in azurerm_data_factory_credential_user_managed_identity.data_factory_credential_user_managed_identities : k => v.identity_id }
+  value       = { for k, v in azurerm_data_factory_credential_user_managed_identity.data_factory_credential_user_managed_identities : k => v.identity_id if v.identity_id != null && length(v.identity_id) > 0 }
 }
 output "data_factory_credential_user_managed_identities_name" {
   description = "Map of name values across all data_factory_credential_user_managed_identities, keyed the same as var.data_factory_credential_user_managed_identities"
-  value       = { for k, v in azurerm_data_factory_credential_user_managed_identity.data_factory_credential_user_managed_identities : k => v.name }
+  value       = { for k, v in azurerm_data_factory_credential_user_managed_identity.data_factory_credential_user_managed_identities : k => v.name if v.name != null && length(v.name) > 0 }
 }
 
